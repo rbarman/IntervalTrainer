a6e6ac15ee729a0e9d1fe53abe48f6a88b58bf40
Rohan Barman <rohan1barman@gmail.com>
2020-08-01 22:27:20 -0500

    gui_layout

--- gui.py
+++ gui.py
@@ -1,40 +1,16 @@
 import PySimpleGUI as sg
 from Sequence import Sequence
+from gui_layout import get_layout
 
 sg.theme('DarkAmber')
-
-train_tab = [
-	[sg.Text('Interval Trainer')],
-	# TODO: instead of text, show png of notes
-	[sg.Text('						',key='-SOLUTION_TEXT-')],      
-	[sg.B('New Sequence'), sg.B('Solution'),sg.B('Replay Sequence')]
-]
-
-settings_tab = [
-	[sg.Text('')],
-	[sg.Text('Root Note:')
-		,sg.Combo([' C',' C#',' D',' D#',' E',' F',' F#',' G',' G#',' A',' A#',' B'],default_value=' C', key='-RootNote-')],
-	[sg.Text('Sequence Length:')
-		,sg.Combo([2,3,4,5,6,7,8],default_value=2,key='-SequenceLength-')],	
-]
-
-layout = [
-	[sg.TabGroup([
-		[sg.Tab('Trainer', train_tab), 
-		sg.Tab('Settings', settings_tab)]]
-	)],    
-]    
-window = sg.Window('Interval Trainer', layout)      
-
-#################################################################
+window = sg.Window('Interval Trainer', get_layout()) 
 
 sequence = None
-
 while True:
 	event, values = window.read() 
 	print(event, values)       
 
-	if event == sg.WIN_CLOSED or event == 'Exit':
+	if event == sg.WIN_CLOSED:
 		break
 
 	elif event == 'New Sequence':
@@ -46,9 +22,7 @@ while True:
 
 	elif event == 'Solution':
 		if sequence is not None:
-			solution = sequence.get_text()
-			print(f'solution: {solution}')
-			window['-SOLUTION_TEXT-'].update(value=solution)
+			window['-SOLUTION_TEXT-'].update(value=sequence.get_text())
 		
 	elif event == 'Replay Sequence':
 		if sequence is not None:
