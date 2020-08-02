8aec2333c47024459f9b81b10867cfd32caef3f1
Rohan Barman <rohan1barman@gmail.com>
2020-08-01 22:15:47 -0500

    settings tab - update root note and sequence length

--- gui.py
+++ gui.py
@@ -3,16 +3,33 @@ from Sequence import Sequence
 
 sg.theme('DarkAmber')
 
-layout = [
-			[sg.Text('Interval Trainer')],
-			# TODO: instead of text, show png of notes
-			[sg.Text('						',key='KEY_SOLUTION_TEXT')],      
-			[sg.B('New Sequence'), sg.B('Solution'),sg.B('Replay Sequence')]
-		]
+train_tab = [
+	[sg.Text('Interval Trainer')],
+	# TODO: instead of text, show png of notes
+	[sg.Text('						',key='-SOLUTION_TEXT-')],      
+	[sg.B('New Sequence'), sg.B('Solution'),sg.B('Replay Sequence')]
+]
 
+settings_tab = [
+	[sg.Text('')],
+	[sg.Text('Root Note:')
+		,sg.Combo([' C',' C#',' D',' D#',' E',' F',' F#',' G',' G#',' A',' A#',' B'],default_value=' C', key='-RootNote-')],
+	[sg.Text('Sequence Length:')
+		,sg.Combo([2,3,4,5,6,7,8],default_value=2,key='-SequenceLength-')],	
+]
+
+layout = [
+	[sg.TabGroup([
+		[sg.Tab('Trainer', train_tab), 
+		sg.Tab('Settings', settings_tab)]]
+	)],    
+]    
 window = sg.Window('Interval Trainer', layout)      
 
+#################################################################
+
 sequence = None
+
 while True:
 	event, values = window.read() 
 	print(event, values)       
@@ -21,15 +38,17 @@ while True:
 		break
 
 	elif event == 'New Sequence':
-		window['KEY_SOLUTION_TEXT'].update(value='						')
-		sequence = Sequence()
+		window['-SOLUTION_TEXT-'].update(value='						')
+		sequence = Sequence(
+			root=window['-RootNote-'].get().strip(), 
+			num_notes = window['-SequenceLength-'].get())
 		sequence.play()
 
 	elif event == 'Solution':
 		if sequence is not None:
 			solution = sequence.get_text()
 			print(f'solution: {solution}')
-			window['KEY_SOLUTION_TEXT'].update(value=solution)
+			window['-SOLUTION_TEXT-'].update(value=solution)
 		
 	elif event == 'Replay Sequence':
 		if sequence is not None:
